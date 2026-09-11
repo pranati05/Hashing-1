5a5769d7aa32be52a6a2b977ac6fd0ba2f25bfb8
Pranati <pranati05@gmail.com>
2026-09-10 19:36:00 -0700

    group anagrams

--- Group_Anagrams.py
+++ Group_Anagrams.py
@@ -19,3 +19,50 @@ class Solution:
     
 #Time - O(NKlogk) k is each word so sorting k words is klogk and N is the number of words in array.
 #Space - O(Nk)
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if not strs:
+            return []
+        dict = {}
+        for str in strs:
+            product = 1
+            for ch in str:
+                product *= self.primes[ch]
+            if product not in dict:
+                dict[product] = [str]
+            else:
+                dict[product].append(str)
+        return list(dict.values())
+
+    def __init__(self):
+        self.primes = {'a': 2, 
+                'b': 3, 
+                'c': 5, 
+                'd': 7, 
+                'e': 11, 
+                'f': 13,
+                'g': 17,
+                'h': 19,
+                'i': 23,
+                'j': 29,
+                'k': 31,
+                'l': 37,
+                'm': 41,
+                'n': 43,
+                'o': 47,
+                'p': 53,
+                'q': 59,
+                'r': 61,
+                's': 67, 
+                't': 71,
+                'u': 73,
+                'v': 79,
+                'w': 83,
+                'x': 89,
+                'y': 97,
+                'z': 101
+                }
+#Time - O(NK)
+#Space - O(NK)
+        
